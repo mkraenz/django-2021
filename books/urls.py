b6111b6db500812ef840d9b9a874b27d85e6b2b1
Mirco Kraenz <kraenzmirco@gmail.com>
2021-10-05 16:47:15 +0200

    refactor(#1): use namespaced urls

--- books/urls.py
+++ books/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'books'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:book_id>/', views.book_details, name='book_details'),
