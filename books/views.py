8601094b5a2aca70a8140e1cb205bff77aa5e5d4
Mirco Kraenz <kraenzmirco@gmail.com>
2021-10-05 16:40:23 +0200

    feat(#1): basic pages for books + chapters

--- books/views.py
+++ books/views.py
@@ -2,32 +2,32 @@
 # Create your views here.
 from typing import Callable
 
-from django.http import HttpRequest, HttpResponse
+from django.http import Http404, HttpRequest, HttpResponse
+from django.shortcuts import get_object_or_404, render
 
 from books.models import Book, Chapter
 
 
 def index(request: HttpRequest) -> HttpResponse:
     books: list[Book] = Book.objects.all()
-    withLink: Callable[[Book],
-                       str] = lambda x: f'<li><a href="/books/{x.id}">{x}</a></li>'
-    book_list = ''.join([withLink(book) for book in books])
-    return HttpResponse(f"<ul>{book_list}</ul>")
+    context = {
+        'books': books,
+    }
+    return render(request, 'books/index.html', context)
 
 
 def book_details(request: HttpRequest, book_id: int) -> HttpResponse:
-    book: Book = Book.objects.get(id=book_id)
-    return HttpResponse(book)
+    book: Book = get_object_or_404(Book, pk=book_id)
+    return render(request, 'books/book-details.html', {'book': book})
 
 
 def chapters(request: HttpRequest, book_id: int) -> HttpResponse:
     book: Book = Book.objects.get(id=book_id)
-    chapters: list[Chapter] = book.chapter_set.all()
-    chapter_list = ', '.join([str(chapter) for chapter in chapters])
-    return HttpResponse(f"<p>{book}</p><p>{chapter_list}</p>")
+    chapters: list[Chapter] = book.chapter_set.all().order_by('numeral')
+    return render(request, 'books/chapters.html', {'book': book, 'chapters': chapters})
 
 
 def chapter_details(request: HttpRequest, book_id: int, chapter_numeral: int) -> HttpResponse:
     book: Book = Book.objects.get(id=book_id)
     chapter: Chapter = book.chapter_set.get(numeral=chapter_numeral)
-    return HttpResponse(f"Chapter {str(chapter)}")
+    return render(request, 'books/chapter-details.html', {'book': book, 'chapter': chapter})
