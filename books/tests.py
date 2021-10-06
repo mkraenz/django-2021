962f2dca0b1131564ffcb73a6cebfd5bf3c469a5
Mirco Kraenz <kraenzmirco@gmail.com>
2021-10-06 09:05:18 +0200

    test(#1): Book was published today

--- books/tests.py
+++ books/tests.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, timedelta
 
 from django.test import TestCase
 
@@ -8,6 +8,16 @@ from books.models import Book
 class BookModelTests(TestCase):
 
     def test_was_published_today_for_today_is_true(self):
-        time = date.today()
-        book = Book(pub_date=time)
+        today = date.today()
+        book = Book(pub_date=today)
         self.assertTrue(book.was_published_today())
+
+    def test_was_published_today_for_yesterday_is_false(self):
+        yesterday = date.today() - timedelta(days=1)
+        book = Book(pub_date=yesterday)
+        self.assertFalse(book.was_published_today())
+
+    def test_was_published_today_for_tomorrow_is_false(self):
+        tomorrow = date.today() + timedelta(days=1)
+        book = Book(pub_date=tomorrow)
+        self.assertFalse(book.was_published_today())
