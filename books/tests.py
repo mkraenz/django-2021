f94fbc6a11b8df300da580d4fe0194332d4664b8
Mirco Kraenz <kraenzmirco@gmail.com>
2021-10-06 09:23:24 +0200

    test(#1): property-based testing with hypothesis

--- books/tests.py
+++ books/tests.py
@@ -1,6 +1,7 @@
 from datetime import date, timedelta
 
-from django.test import TestCase
+from hypothesis import given, strategies
+from hypothesis.extra.django import TestCase
 
 from books.models import Book
 
@@ -12,12 +13,14 @@ class BookModelTests(TestCase):
         book = Book(pub_date=today)
         self.assertTrue(book.was_published_today())
 
-    def test_was_published_today_for_yesterday_is_false(self):
-        yesterday = date.today() - timedelta(days=1)
-        book = Book(pub_date=yesterday)
+    @given(strategies.integers(1, 1000))
+    def test_was_published_today_for_future_dates_is_false(self, days: int):
+        future = date.today() + timedelta(days=days)
+        book = Book(pub_date=future)
         self.assertFalse(book.was_published_today())
 
-    def test_was_published_today_for_tomorrow_is_false(self):
-        tomorrow = date.today() + timedelta(days=1)
-        book = Book(pub_date=tomorrow)
+    @given(strategies.integers(-1000, -1))
+    def test_was_published_today_for_past_dates_is_false(self, days: int):
+        past = date.today() - timedelta(days=days)
+        book = Book(pub_date=past)
         self.assertFalse(book.was_published_today())
