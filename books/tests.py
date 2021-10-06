8a13b6dccafe24ebb34381c6d8c42664c046b059
Mirco Kraenz <kraenzmirco@gmail.com>
2021-10-06 09:00:26 +0200

    build(#1): vsode django debug tests config

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,13 @@
+from datetime import date
+
 from django.test import TestCase
 
-# Create your tests here.
+from books.models import Book
+
+
+class BookModelTests(TestCase):
+
+    def test_was_published_today_for_today_is_true(self):
+        time = date.today()
+        book = Book(pub_date=time)
+        self.assertTrue(book.was_published_today())
